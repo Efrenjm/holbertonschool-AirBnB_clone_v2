dc711dffd58cf7474e1c6bb86508fdc5d783eae3
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-03-29 00:54:51 -0600

    fix typo

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,5 @@
 -- Create hbnb_dev_db, hbnb_dev user and grant proper permissions
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
