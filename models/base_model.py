d1febc227c6ea7d03fad1a01f3cadf13e223061f
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-03-29 15:58:39 -0600

    new test

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # storage.new(self)
+            storage.new(self)
         else:
             for key, val in kwargs.items():
                 if key != "__class__":
@@ -30,6 +30,8 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
+            if not hasattr(self, 'id'):
+                self.id = str(uuid.uuid4())
             self.__dict__.update(kwargs)
 
     def __str__(self):
