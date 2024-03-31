369dde84fc764ab5aee2cb916b15dd7c9d0734af
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-03-30 21:02:52 -0600

    new test

--- models/base_model.py
+++ models/base_model.py
@@ -25,19 +25,20 @@ class BaseModel:
         else:
             for key, val in kwargs.items():
                 if key != "__class__":
-                    kwargs[key] = val
-            if self.created_at is None:
-                self.created_at = datetime.now()\
-                    .strftime('%Y-%m-%dT%H:%M:%S.%f')
-            if self.updated_at is None:
-                self.updated_at = datetime.now()\
-                    .strftime('%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(self.updated_at,
-                                                '%Y-%m-%dT%H:%M:%S.%f')
-            self.created_at = datetime.strptime(self.created_at,
-                                                '%Y-%m-%dT%H:%M:%S.%f')
-            if not hasattr(kwargs, 'id'):
-                kwargs['id'] = str(uuid.uuid4())
+                    setattr(self, key, val)
+            try:
+                kwargs['updated_at'] = datetime.\
+                    strptime(kwargs['updated_at'],
+                             '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.\
+                    strptime(kwargs['created_at'],
+                             '%Y-%m-%dT%H:%M:%S.%f')
+                del kwargs['__class__']
+            except KeyError:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+
             self.__dict__.update(kwargs)
 
     def __str__(self):
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" Unittest for class State"""
 from tests.test_models.test_base_model import test_basemodel
-from models.review import Review
+from models.state import State
 
 
-class test_review(test_basemodel):
-    """ """
+class test_state(test_basemodel):
+    """ Unittest of State class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ test of initialisation """
         super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+        self.name = "State"
+        self.value = State
 
-    def test_place_id(self):
-        """ """
+    def test_name3(self):
+        """ test of type name attribute """
         new = self.value()
-        self.assertEqual(type(new.place_id), str)
-
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
-
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertEqual(type(new.name), str)
