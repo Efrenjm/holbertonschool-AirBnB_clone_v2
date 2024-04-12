6549a8b55a9550986e1c278a3d7b7a82ed7547a4
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-04-12 00:11:55 -0600

    new new test

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,10 +37,11 @@ def show_number(n):
 
 @app.route('/number_template(<int:n>)', strict_slashes=False)
 def show_template(n):
-    # if isinstance(n, int):
+    if n.isdigit():
+        n= int(n)
         return render_template('5-number.html', title="HBNB", number=n)
-    # else:
-        # abort(404)
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
