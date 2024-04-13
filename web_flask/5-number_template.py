b313a02e118ef8a9ea6cefb6c9775644154c820b
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-04-12 21:30:20 -0600

    fix typo

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def show_number(n):
     return "{} is a number".format(n)
 
 
-@app.route('/number_template(<int:n>)', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def show_template(n):
     return render_template('5-number.html', number=n)
 
